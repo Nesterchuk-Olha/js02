b4c2109dc6b71a8674bbb1adadc65b6b4b766087
Nesterchuk-Olha <132381940+Nesterchuk-Olha@users.noreply.github.com>
2023-05-25 20:15:57 +0300

    Update js_02.js

--- js_02.js
+++ js_02.js
@@ -6,11 +6,13 @@ alert (b);
 //Number:temperature
 let a = prompt('Яка зараз температура у Цельсіях?');
 b = (a*1.8)+32;
+console.log (b);
 
 //Number:divide
 let a = prompt('Введіть ділене');
 let b = prompt('Введіть дільник');
 c = Math.floor(a/b);
+console.log (c);
 
 //Number: currency
 const rate = 36.95; //гривень коштує 1 долар
@@ -23,10 +25,13 @@ const red = prompt('Введіть перший колір');
 const green = prompt('Введіть другий колір');
 const blue = prompt('Введіть третій колір');
 color = ('#'+red+green+blue);
+console.log (color);
 
 //Numbers: flats
 let a = prompt('Введіть кількість поверхів');
 let b = prompt('Введіть кількість квартир на поверсі');
 let c = prompt('Введіть номер квартири');
 d = Math.ceil(c/(a*b)); //номер під'їзду
-e = c%(a*b);
+e = Math.ceil((c - (d-1)*a*b)/b);
+console.log (d);
+console.log (e);
